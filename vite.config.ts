2436137d416ae4c85169ba3ad406d42e2cd2c40d
2000K <271157854+REC2000K@users.noreply.github.com>
2026-04-13 09:59:50 +0500

    commit 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Market/',
+  base: '/RevitMarket/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
